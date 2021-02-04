4e81a4dea172e76c72c3d5212f9e53472b22a3e8
jiashunx <wenjiashun@163.com>
2021-02-04 21:31:00 +0800

    create and save article.

--- src/main/java/io/github/jiashunx/app/miniblog/console/ArticleManageServlet.java
+++ src/main/java/io/github/jiashunx/app/miniblog/console/ArticleManageServlet.java
@@ -11,9 +11,12 @@ import io.github.jiashunx.masker.rest.framework.cons.Constants;
 import io.github.jiashunx.masker.rest.framework.servlet.AbstractRestServlet;
 import io.github.jiashunx.masker.rest.framework.servlet.Servlet;
 import io.github.jiashunx.masker.rest.framework.servlet.mapping.GetMapping;
+import io.github.jiashunx.masker.rest.framework.servlet.mapping.PostMapping;
 import io.github.jiashunx.masker.rest.framework.util.IOUtils;
 import io.github.jiashunx.masker.rest.framework.util.MRestHeaderBuilder;
+import io.github.jiashunx.masker.rest.framework.util.StringUtils;
 
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 /**
@@ -31,11 +34,52 @@ public class ArticleManageServlet extends AbstractRestServlet {
         this.articleService = Objects.requireNonNull(serviceBus.getArticleService());
     }
 
+    @PostMapping(url = "/console/article/edit")
+    public void actionEdit(MRestRequest request, MRestResponse response) {
+        Map<String, Object> params = (Map<String, Object>) request.parseBodyToObj(Map.class);
+        String articleId = params.get("articleId").toString();
+        String articleName = params.get("articleName").toString();
+        String articleContent = params.get("articleContent").toString();
+        String articleIdLocator = params.get("articleIdLocator").toString();
+        String articleDescription = params.get("articleDescription").toString();
+        // 新增
+        if (StringUtils.isBlank(articleId)) {
+            ArticleEntity entity = new ArticleEntity();
+            entity.setArticleId(UUID.randomUUID().toString());
+            entity.setArticleName(articleName);
+            entity.setArticleContent(articleContent.getBytes(StandardCharsets.UTF_8));
+            entity.setArticleIdLocator(articleIdLocator);
+            entity.setArticleDescription(articleDescription);
+            entity.setCreateTime(new Date());
+            entity.setLastModifiedTime(entity.getCreateTime());
+            articleService.insert(entity);
+        } else {
+            // 更新
+            ArticleEntity entity = articleService.find(articleId);
+            entity.setArticleName(articleName);
+            entity.setArticleDescription(articleDescription);
+            entity.setArticleContent(articleContent.getBytes(StandardCharsets.UTF_8));
+            entity.setArticleIdLocator(articleIdLocator);
+            entity.setArticleDescription(articleDescription);
+            entity.setLastModifiedTime(new Date());
+            articleService.update(entity);
+        }
+    }
+
+    @PostMapping(url = "/console/article/delete")
+    public void actionDelete(MRestRequest request, MRestResponse response) {
+
+    }
+
     @GetMapping(url = "/console/article/create.html")
     public void createArticle(MRestRequest request, MRestResponse response) {
         Kv kv = new Kv();
         kv.put("status", "新建文章");
-        kv.put("status_code", "new");
+        kv.put("articleId", "");
+        kv.put("articleName", "");
+        kv.put("articleContent", "");
+        kv.put("articleIdLocator", "");
+        kv.put("articleDescription", "");
         response.write(BlogUtils.render(ARTICLE_EDIT_HTML, kv)
                 , MRestHeaderBuilder.Build(Constants.HTTP_HEADER_CONTENT_TYPE, Constants.CONTENT_TYPE_TEXT_HTML));
     }
@@ -45,7 +89,11 @@ public class ArticleManageServlet extends AbstractRestServlet {
         // TODO 获取待编辑的数据
         Kv kv = new Kv();
         kv.put("status", "编辑文章");
-        kv.put("status_code", "edit");
+        kv.put("articleId", "");
+        kv.put("articleName", "");
+        kv.put("articleContent", "");
+        kv.put("articleIdLocator", "");
+        kv.put("articleDescription", "");
         response.write(BlogUtils.render(ARTICLE_EDIT_HTML, kv)
                 , MRestHeaderBuilder.Build(Constants.HTTP_HEADER_CONTENT_TYPE, Constants.CONTENT_TYPE_TEXT_HTML));
     }
