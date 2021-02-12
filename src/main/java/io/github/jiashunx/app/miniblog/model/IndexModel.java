6da627592e32c17969ebca97f348ed54a81e5089
jiashunx <wenjiashun@163.com>
2021-02-12 22:27:00 +0800

    添加文章列表页Service

---
+++ src/main/java/io/github/jiashunx/app/miniblog/model/IndexModel.java
@@ -0,0 +1,17 @@
+package io.github.jiashunx.app.miniblog.model;
+
+/**
+ * @author jiashunx
+ */
+public class IndexModel {
+
+    private int pageIndex;
+
+    public int getPageIndex() {
+        return pageIndex;
+    }
+
+    public void setPageIndex(int pageIndex) {
+        this.pageIndex = pageIndex;
+    }
+}
---
+++ src/main/java/io/github/jiashunx/app/miniblog/service/IndexService.java
@@ -0,0 +1,14 @@
+package io.github.jiashunx.app.miniblog.service;
+
+import io.github.jiashunx.app.miniblog.model.IndexModel;
+
+/**
+ * @author jiashunx
+ */
+public class IndexService {
+
+    public void index(IndexModel indexModel) {
+        // TODO 首页、文章列表页、tag文章列表页、分类文章列表页、全部使用此方法进行响应数据封装
+    }
+
+}
