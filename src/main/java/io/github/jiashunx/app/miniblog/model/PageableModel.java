732d76e5a3da1f7b74ca243ca220f8e091fc86cd
jiashunx <wenjiashun@163.com>
2021-02-13 22:20:00 +0800

    添加分页模型.

---
+++ src/main/java/io/github/jiashunx/app/miniblog/model/PageableModel.java
@@ -0,0 +1,39 @@
+package io.github.jiashunx.app.miniblog.model;
+
+import io.github.jiashunx.app.miniblog.model.entity.PageIndex;
+
+import java.util.List;
+
+/**
+ * @author jiashunx
+ */
+public class PageableModel {
+
+    private boolean prevEnabled;
+    private List<PageIndex> pageIndexList;
+    private boolean nextEnabled;
+
+    public boolean isPrevEnabled() {
+        return prevEnabled;
+    }
+
+    public void setPrevEnabled(boolean prevEnabled) {
+        this.prevEnabled = prevEnabled;
+    }
+
+    public List<PageIndex> getPageIndexList() {
+        return pageIndexList;
+    }
+
+    public void setPageIndexList(List<PageIndex> pageIndexList) {
+        this.pageIndexList = pageIndexList;
+    }
+
+    public boolean isNextEnabled() {
+        return nextEnabled;
+    }
+
+    public void setNextEnabled(boolean nextEnabled) {
+        this.nextEnabled = nextEnabled;
+    }
+}
---
+++ src/main/java/io/github/jiashunx/app/miniblog/model/entity/PageIndex.java
@@ -0,0 +1,55 @@
+package io.github.jiashunx.app.miniblog.model.entity;
+
+/**
+ * @author jiashunx
+ */
+public class PageIndex {
+    /**
+     * 分页是否忽略(...)
+     */
+    private boolean ignore;
+    /**
+     * 分页数字
+     */
+    private int indexNo;
+    /**
+     * url.
+     */
+    private String indexUrl;
+    /**
+     * 是否当前展示页
+     */
+    private boolean current;
+
+    public boolean isIgnore() {
+        return ignore;
+    }
+
+    public void setIgnore(boolean ignore) {
+        this.ignore = ignore;
+    }
+
+    public int getIndexNo() {
+        return indexNo;
+    }
+
+    public void setIndexNo(int indexNo) {
+        this.indexNo = indexNo;
+    }
+
+    public String getIndexUrl() {
+        return indexUrl;
+    }
+
+    public void setIndexUrl(String indexUrl) {
+        this.indexUrl = indexUrl;
+    }
+
+    public boolean isCurrent() {
+        return current;
+    }
+
+    public void setCurrent(boolean current) {
+        this.current = current;
+    }
+}
