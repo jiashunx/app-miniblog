628f9146a71e52fcd16646c45c4468bf04d43db7
jiashunx <wenjiashun@163.com>
2021-02-16 22:57:00 +0800

    根据locatorId与创建时间查找对应文章实体模型.

--- src/main/java/io/github/jiashunx/app/miniblog/service/ArticleService.java
+++ src/main/java/io/github/jiashunx/app/miniblog/service/ArticleService.java
@@ -1,8 +1,11 @@
 package io.github.jiashunx.app.miniblog.service;
 
 import io.github.jiashunx.app.miniblog.model.entity.ArticleEntity;
+import io.github.jiashunx.app.miniblog.util.BlogUtils;
 import io.github.jiashunx.tools.sqlite3.service.SQLite3Service;
 import io.github.jiashunx.tools.sqlite3.util.SQLite3Utils;
+
+import java.util.List;
 
 /**
  * @author jiashunx
@@ -23,6 +26,17 @@ public class ArticleService extends SQLite3Service<ArticleEntity, String> implem
         return SQLite3Utils.getClassTableModel(getEntityClass()).getSelectAllSQL(stringBuilder -> {
             stringBuilder.append(" ORDER BY CREATE_TIME DESC");
         });
+    }
+
+    public ArticleEntity findByLocatorIdAndDate(String dateStr, String locatorId) {
+        List<ArticleEntity> entityList = listAll();
+        for (ArticleEntity entity: entityList) {
+            if (locatorId.equals(entity.getArticleIdLocator())
+                && BlogUtils.format(entity.getCreateTime(), BlogUtils.yyyyMMdd).equals(dateStr)) {
+                return entity;
+            }
+        }
+        return null;
     }
 
     @Override
