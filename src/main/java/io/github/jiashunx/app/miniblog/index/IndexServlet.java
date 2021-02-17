46fcb56154b0f67252e2e9f6068c3a0f5a3bdeb1
jiashunx <wenjiashun@163.com>
2021-02-17 22:57:00 +0800

    分页计算处理

--- src/main/java/io/github/jiashunx/app/miniblog/index/IndexServlet.java
+++ src/main/java/io/github/jiashunx/app/miniblog/index/IndexServlet.java
@@ -1,11 +1,20 @@
 package io.github.jiashunx.app.miniblog.index;
 
-import io.github.jiashunx.app.miniblog.service.ServiceBus;
+import io.github.jiashunx.app.miniblog.model.IndexModel;
+import io.github.jiashunx.app.miniblog.model.PageableIndex;
+import io.github.jiashunx.app.miniblog.model.entity.ArticleEntity;
+import io.github.jiashunx.app.miniblog.service.*;
 import io.github.jiashunx.masker.rest.framework.MRestRequest;
 import io.github.jiashunx.masker.rest.framework.MRestResponse;
 import io.github.jiashunx.masker.rest.framework.filter.Filter;
 import io.github.jiashunx.masker.rest.framework.filter.MRestFilter;
 import io.github.jiashunx.masker.rest.framework.filter.MRestFilterChain;
+import io.github.jiashunx.tools.sqlite3.SQLite3JdbcTemplate;
+import io.netty.handler.codec.http.HttpResponseStatus;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * @author jiashunx
@@ -13,47 +22,117 @@ import io.github.jiashunx.masker.rest.framework.filter.MRestFilterChain;
 @Filter(urlPatterns = "/*", order = Integer.MAX_VALUE)
 public class IndexServlet implements MRestFilter {
 
+    private final SQLite3JdbcTemplate sqLite3JdbcTemplate;
+    private final ArticleService articleService;
+    private final CategoryService categoryService;
+    private final ArticleCategoryService articleCategoryService;
+    private final TagService tagService;
+    private final ArticleTagService articleTagService;
+
+    public IndexServlet(ServiceBus serviceBus) {
+        this.sqLite3JdbcTemplate = serviceBus.getDatabaseService().getJdbcTemplate();
+        this.articleService = Objects.requireNonNull(serviceBus.getArticleService());
+        this.categoryService = Objects.requireNonNull(serviceBus.getCategoryService());
+        this.articleCategoryService = Objects.requireNonNull(serviceBus.getArticleCategoryService());
+        this.tagService = Objects.requireNonNull(serviceBus.getTagService());
+        this.articleTagService = Objects.requireNonNull(serviceBus.getArticleTagService());
+    }
+
     @Override
     public void doFilter(MRestRequest request, MRestResponse response, MRestFilterChain filterChain) {
         String requestUrl = request.getUrl();
         switch (requestUrl) {
             case "/":
-                root(request, response);
-                return;
+                index(1, request, response);
+                break;
             case "/timeline":
                 timeline(request, response);
-                return;
+                break;
             case "/categories":
                 categories(request, response);
-                return;
+                break;
             case "/tags":
                 tags(request, response);
-                return;
+                break;
             default:
                 //    /2021/02/23/article-short-id
                 if (requestUrl.matches("^/\\d{4}/\\d{1,2}/\\d{1,2}/\\S+$")) {
-                    return;
+
                 } else if (requestUrl.matches("^/tag/\\S+$")) {
                     //    /tags/tag-name
-                    return;
                 } else if (requestUrl.matches("^/categories/\\S+$")) {
                     //    /categories/category-name
-                    return;
                 } else if (requestUrl.matches("^/page/\\d+$")) {
                     //    /page/page-index
-                    return;
+                    int pageIndex = Integer.parseInt(requestUrl.substring(requestUrl.lastIndexOf("/") + 1));
+                    index(pageIndex, request, response);
                 } else {
                     filterChain.doFilter(request, response);
                 }
-
+                break;
         }
     }
 
-    public IndexServlet(ServiceBus serviceBus) {
+    public void index(int pageIndex, MRestRequest request, MRestResponse response) {
+        List<ArticleEntity> entityList = articleService.listAll();
+        int totalSize = entityList.size();
+        int pageRowCount = 20;
+        int pageSize = (totalSize / pageRowCount) + ((totalSize % pageRowCount) > 0 ? 1 : 0);
+        if (pageIndex < 1 || pageIndex > pageSize) {
+            response.writeStatusPageAsHtml(HttpResponseStatus.NOT_FOUND);
+            return;
+        }
 
-    }
+        IndexModel indexModel = new IndexModel();
+        indexModel.setPrevEnabled(pageIndex > 1);
+        indexModel.setNextEnabled(pageIndex < pageSize);
+        List<PageableIndex> pageableIndexList = new ArrayList<>();
+        PageableIndex currentPageableIndex = new PageableIndex();
+        currentPageableIndex.setCurrent(true);
+        currentPageableIndex.setIndexNo(pageIndex);
+        if (pageIndex - 1 > 3) {
+            PageableIndex firstPageableIndex = new PageableIndex();
+            firstPageableIndex.setIndexNo(1);
+            firstPageableIndex.setIndexUrl("/page/1");
+            PageableIndex ignorePageableIndex = new PageableIndex();
+            ignorePageableIndex.setIgnore(true);
+            pageableIndexList.add(firstPageableIndex);
+            pageableIndexList.add(ignorePageableIndex);
+            for (int index = pageIndex - 2; index < pageIndex; index++) {
+                PageableIndex pageableIndex = new PageableIndex();
+                pageableIndex.setIndexNo(index);
+                pageableIndex.setIndexUrl("/page/" + index);
+                pageableIndexList.add(pageableIndex);
+            }
+        } else {
+            for (int index = 1; index < pageIndex; index++) {
+                PageableIndex pageableIndex = new PageableIndex();
+                pageableIndex.setIndexNo(index);
+                pageableIndex.setIndexUrl("/page/" + index);
+                pageableIndexList.add(pageableIndex);
+            }
+        }
+        pageableIndexList.add(currentPageableIndex);
+        if (pageSize - pageIndex > 3) {
+            for (int index = pageIndex + 1; index < pageIndex + 3; index++) {
+                PageableIndex pageableIndex = new PageableIndex();
+                pageableIndex.setIndexNo(index);
+                pageableIndex.setIndexUrl("/page/" + index);
+                pageableIndexList.add(pageableIndex);
+            }
+            PageableIndex ignorePageableIndex = new PageableIndex();
+            ignorePageableIndex.setIgnore(true);
+            pageableIndexList.add(ignorePageableIndex);
+        } else {
+            for (int index = pageIndex + 1; index <= pageSize; index++) {
+                PageableIndex pageableIndex = new PageableIndex();
+                pageableIndex.setIndexNo(index);
+                pageableIndex.setIndexUrl("/page/" + index);
+                pageableIndexList.add(pageableIndex);
+            }
+        }
+        indexModel.setPageableIndexList(pageableIndexList);
 
-    public void root(MRestRequest request, MRestResponse response) {
     }
 
     /**
